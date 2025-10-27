1a79d7475367a5b305d1b682cbf08fba4d9410a8
KingRavo18 <rkingovskis14@gmail.com>
2025-10-27 21:26:44 +0200

    f f

--- public/dist/sign_in.js
+++ public/dist/sign_in.js
@@ -1,17 +1,20 @@
-function changeForm(currentFormId, nextFormId, currentFormAppearClass, currentFormDisappearClass, nextFormAppearClass) {
+function changeForm(currentFormId, nextFormId, nextFormTriggerId, currentFormAppearClass, currentFormDisappearClass, nextFormAppearClass) {
     const currentForm = document.getElementById(currentFormId);
     const nextForm = document.getElementById(nextFormId);
+    const nextFormTrigger = document.getElementById(nextFormTriggerId);
     currentForm.classList.remove(currentFormAppearClass);
     currentForm.classList.add(currentFormDisappearClass);
     nextForm.classList.add(nextFormAppearClass);
     nextForm.style.display = "block";
+    nextFormTrigger.classList.add("click-disabled");
     currentForm.addEventListener("animationend", () => {
         currentForm.classList.remove(currentFormDisappearClass);
         currentForm.style.display = "none";
+        nextFormTrigger.classList.remove("click-disabled");
     }, { once: true });
 }
-document.getElementById("change-form-trigger-signin").onclick = () => changeForm("signin_container", "registration_container", "right-form-appear-animation", "right-form-disappear-animation", "left-form-appear-animation");
-document.getElementById("change-form-trigger-register").onclick = () => changeForm("registration_container", "signin_container", "left-form-appear-animation", "left-form-disappear-animation", "right-form-appear-animation");
+document.getElementById("change-form-trigger-signin").onclick = () => changeForm("signin_container", "registration_container", "change-form-trigger-register", "right-form-appear-animation", "right-form-disappear-animation", "left-form-appear-animation");
+document.getElementById("change-form-trigger-register").onclick = () => changeForm("registration_container", "signin_container", "change-form-trigger-signin", "left-form-appear-animation", "left-form-disappear-animation", "right-form-appear-animation");
 function togglePasswordVisibility(trigger_id, triggered_input_id) {
     const trigger = document.getElementById(trigger_id);
     const triggeredInput = document.getElementById(triggered_input_id);
--- public/src/sign_in.ts
+++ public/src/sign_in.ts
@@ -1,26 +1,32 @@
 function changeForm(
     currentFormId: string, 
     nextFormId: string, 
+    nextFormTriggerId: string,
     currentFormAppearClass: string, 
     currentFormDisappearClass: string, 
     nextFormAppearClass: string
+    
 ): void{
     const currentForm = document.getElementById(currentFormId) as HTMLElement;
     const nextForm = document.getElementById(nextFormId) as HTMLElement;
+    const nextFormTrigger = document.getElementById(nextFormTriggerId) as HTMLElement;
 
     currentForm.classList.remove(currentFormAppearClass);
     currentForm.classList.add(currentFormDisappearClass);
     nextForm.classList.add(nextFormAppearClass);
     nextForm.style.display = "block";
+    nextFormTrigger.classList.add("click-disabled"); 
     currentForm.addEventListener("animationend", () => {
         currentForm.classList.remove(currentFormDisappearClass);
         currentForm.style.display = "none";
+        nextFormTrigger.classList.remove("click-disabled");
     }, {once: true});
     
 }
 (document.getElementById("change-form-trigger-signin") as HTMLElement).onclick = () => changeForm(
     "signin_container", 
     "registration_container", 
+    "change-form-trigger-register",
     "right-form-appear-animation", 
     "right-form-disappear-animation", 
     "left-form-appear-animation"
@@ -28,9 +34,10 @@ function changeForm(
 (document.getElementById("change-form-trigger-register") as HTMLElement).onclick = () => changeForm(
     "registration_container", 
     "signin_container", 
+    "change-form-trigger-signin",
     "left-form-appear-animation", 
     "left-form-disappear-animation", 
-    "right-form-appear-animation"
+    "right-form-appear-animation"    
 );
 
 function togglePasswordVisibility(
