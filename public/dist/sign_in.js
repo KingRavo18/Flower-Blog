0da1f297c16e835044bdc33947a04f8d03f16af7
KingRavo18 <rkingovskis14@gmail.com>
2025-10-27 09:09:27 +0200

    d

--- public/dist/sign_in.js
+++ public/dist/sign_in.js
@@ -1,5 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const { openLeftForm, openRightForm } = changeForm();
+    document.getElementById("change-form-trigger-signin").onclick = () => openLeftForm();
+    document.getElementById("change-form-trigger-register").onclick = () => openRightForm();
+});
 function changeForm() {
+    const leftForm = document.getElementById("registration_container");
+    const rightForm = document.getElementById("signin_container");
+    function openLeftForm() {
+        rightForm.classList.remove("right-form-appear-animation");
+        rightForm.classList.add("right-form-disappear-animation");
+        leftForm.classList.add("left-form-appear-animation");
+        leftForm.style.display = "block";
+        setTimeout(() => {
+            rightForm.style.display = "none";
+            rightForm.classList.remove("right-form-disappear-animation");
+        }, 1900);
+    }
+    function openRightForm() {
+        leftForm.classList.remove("left-form-appear-animation");
+        leftForm.classList.add("left-form-disappear-animation");
+        rightForm.classList.add("right-form-appear-animation");
+        rightForm.style.display = "block";
+        setTimeout(() => {
+            leftForm.style.display = "none";
+            leftForm.classList.remove("left-form-disappear-animation");
+        }, 1900);
+    }
+    return { openLeftForm, openRightForm };
 }
-document.getElementById("change-form-text").onclick = () => openLeftForm();
 export {};
 //# sourceMappingURL=sign_in.js.map
--- public/src/sign_in.ts
+++ public/src/sign_in.ts
@@ -1,3 +1,38 @@
-function changeForm(){
+document.addEventListener("DOMContentLoaded", () => {
+    const {openLeftForm, openRightForm} = changeForm();
+    (document.getElementById("change-form-trigger-signin") as HTMLElement).onclick = () => openLeftForm();
+    (document.getElementById("change-form-trigger-register") as HTMLElement).onclick = () => openRightForm();
+});
+
+interface changeFormReturnTypes{
+    openLeftForm: () => void;
+    openRightForm: () => void;
 }
-(document.getElementById("change-form-text") as HTMLElement).onclick = () => openLeftForm();
+function changeForm(): changeFormReturnTypes{
+    const leftForm = document.getElementById("registration_container") as HTMLElement;
+    const rightForm = document.getElementById("signin_container") as HTMLElement;
+
+    function openLeftForm(): void{
+        rightForm.classList.remove("right-form-appear-animation");
+        rightForm.classList.add("right-form-disappear-animation");
+        leftForm.classList.add("left-form-appear-animation");
+        leftForm.style.display = "block";
+        setTimeout(() => {
+            rightForm.style.display = "none";
+            rightForm.classList.remove("right-form-disappear-animation");
+        }, 1900);
+    }
+
+    function openRightForm(): void{
+        leftForm.classList.remove("left-form-appear-animation");
+        leftForm.classList.add("left-form-disappear-animation");
+        rightForm.classList.add("right-form-appear-animation");
+        rightForm.style.display = "block";
+        setTimeout(() => {
+            leftForm.style.display = "none";
+            leftForm.classList.remove("left-form-disappear-animation");
+        }, 1900);
+    }
+
+    return {openLeftForm, openRightForm}
+}
