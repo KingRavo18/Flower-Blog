78d444d4350745b77053d0672139513a78b5e673
KingRavo18 <rkingovskis14@gmail.com>
2025-10-31 15:50:19 +0200

    d

---
+++ public/dist/global.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchNavbar();
+}, { once: true });
+async function fetchNavbar() {
+    try {
+        const response = await fetch("./assets/components/nav-bar.html");
+        if (!response.ok) {
+            throw new Error("Could not find the navigation bar.");
+        }
+        const data = await response.text();
+        document.getElementById("navigation-bar").innerHTML = data;
+    }
+    catch (error) {
+        console.error();
+    }
+}
+export {};
+//# sourceMappingURL=global.js.map
---
+++ public/src/global.ts
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchNavbar();
+}, {once: true});
+
+async function fetchNavbar(){
+    try{
+        const response = await fetch("./assets/components/nav-bar.html");
+        if(!response.ok){
+            throw new Error("Could not find the navigation bar.");
+        }
+        const data = await response.text();
+        (document.getElementById("navigation-bar") as HTMLElement).innerHTML = data;
+    }
+    catch(error){
+        console.error();
+    }
+}
