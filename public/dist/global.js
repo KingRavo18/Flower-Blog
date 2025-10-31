970c080db4e8d87d2ecd637530f3ee22e14c2661
KingRavo18 <rkingovskis14@gmail.com>
2025-10-31 23:29:48 +0200

    f

--- public/dist/global.js
+++ public/dist/global.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchNavbar();
 }, { once: true });
 async function fetchNavbar() {
+    const header = document.getElementById("navigation-bar");
+    if (!header) {
+        return;
+    }
     try {
         const response = await fetch("./assets/components/nav-bar.html");
         if (!response.ok) {
             throw new Error("Could not find the navigation bar.");
         }
         const data = await response.text();
-        document.getElementById("navigation-bar").innerHTML = data;
+        header.innerHTML = data;
     }
     catch (error) {
         console.error(error.message);
--- public/src/global.ts
+++ public/src/global.ts
@@ -3,13 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
 }, {once: true});
 
 async function fetchNavbar(){
+    const header = document.getElementById("navigation-bar") as HTMLElement;
+    if(!header){
+        return;
+    }
     try{
         const response = await fetch("./assets/components/nav-bar.html");
         if(!response.ok){
             throw new Error("Could not find the navigation bar.");
         }
         const data = await response.text();
-        (document.getElementById("navigation-bar") as HTMLElement).innerHTML = data;
+        header.innerHTML = data;
     }
     catch(error){
         console.error((error as Error).message);
